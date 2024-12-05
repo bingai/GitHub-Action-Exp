fb6c42138b3afd92ccf4cdef037d77fb0543df0d
bingai <bingai@users.noreply.github.com>
2024-12-04 23:41:03 -0800

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,11 @@
 # app.py
 # This is a test commit
 def add(a, b):
+    print("add func")
     return a + b
 
 def test_add():
+    print("test add func: case 1")
     assert add(1, 2) == 3
+    print("test add func: case 2")
     assert add(1, -1) == 0
